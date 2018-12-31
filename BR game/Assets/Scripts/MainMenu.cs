f3245a4a9fb343b375cc95c4c4d5f5037c2c6a21
MasterKronos0107 <43812692+MasterKronos0107@users.noreply.github.com>
2018-12-31 14:48:45 +1030

    The main menu and all of it's buttons work.
    Working on the options now. By this I mean the functions. Then a pause menu.

---
+++ BR game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // this can be changed by using the build method
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("You have closed the program"); 
+        Application.Quit(); // shuts down the program
+    }
+
+}

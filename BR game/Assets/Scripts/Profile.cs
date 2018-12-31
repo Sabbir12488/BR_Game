90fa07f65cb30b1656a4355164826dd4064ec0ba
MasterKronos0107 <43812692+MasterKronos0107@users.noreply.github.com>
2019-01-01 08:56:19 +1030

    made a kill system
    the kill system works there is just a issue with how the camera is on the player. Meaning I can't destory the player while keeping the camera alive. Also can we dicuss the player controls.

--- BR game/Assets/Scripts/Profile.cs
+++ BR game/Assets/Scripts/Profile.cs
@@ -3,22 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Profile : MonoBehaviour {
 
     public float health = 100f;
-    public float shileld = 100f;
+    public float shield = 100f;
+    public bool takenDamage = false;
+    public Text deathscreen; 
 
     [SerializeField]private Image healthImage;
     [SerializeField]private Image shieldImage;
 
     // Use this for initialization
     void Start () {
-		
+        deathscreen.enabled = false; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
         healthImage.fillAmount = health / 100;
-        shieldImage.fillAmount = shileld / 100;
+        shieldImage.fillAmount = shield / 100;
+
+
+        
+        if(health <= 0)
+        {
+            Die(); 
+        }
+
 	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemies")
+        {
+            takenDamage = true;
+            takenDamage = false;
+            shield -= 10;
+
+            Debug.Log(shield); 
+           
+        }
+        if(collision.gameObject.tag == "Bullets")
+        {
+            takenDamage = true;
+            takenDamage = false;
+            shield -= 5; 
+     
+        }
+        if (takenDamage = true && shield <= 0)
+        {
+            if(collision.gameObject.tag == "Enemies")
+            {
+                health -= 40; 
+            }
+            if(collision.gameObject.tag == "Bullets")
+            {
+                health -= 10;
+            }
+
+           
+        }
+
+
+    }
+
+    void Die()
+    {
+        deathscreen.enabled = true; 
+        Destroy(gameObject); 
+    }
 }

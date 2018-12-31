9af09c74d9c987d0ce481adc1fa619dab4bd1523
MasterKronos0107 <43812692+MasterKronos0107@users.noreply.github.com>
2018-12-31 19:33:19 +1030

    The menu options all work.
    I am done and shall be working on a pause menu. Once I am done that I then will be doing something a bit special. It is a surprise.

---
+++ BR game/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+
+    public Dropdown resolutionsDropDown; 
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionsDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0; 
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option); 
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i; 
+            }
+        }
+
+        resolutionsDropDown.AddOptions(options);
+        resolutionsDropDown.value = currentResolutionIndex;
+        resolutionsDropDown.RefreshShownValue(); 
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("sound", volume); 
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex); 
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen; 
+    }
+}

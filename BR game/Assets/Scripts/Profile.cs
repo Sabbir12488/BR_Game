550b384552e1cdba226c14919192a41d0936725b
Sabbir12488 <sabbir12488@gmail.com>
2018-12-31 16:22:52 +0600

    health and shield UI

---
+++ BR game/Assets/Scripts/Profile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Profile : MonoBehaviour {
+
+    public float health = 100f;
+    public float shileld = 100f;
+
+    [SerializeField]private Image healthImage;
+    [SerializeField]private Image shieldImage;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthImage.fillAmount = health / 100;
+        shieldImage.fillAmount = shileld / 100;
+	}
+}

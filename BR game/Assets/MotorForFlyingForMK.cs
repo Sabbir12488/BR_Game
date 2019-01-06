0c54815dbb43b4d31f42123f7e93e3f0fc567b9e
MasterKronos0107 <43812692+MasterKronos0107@users.noreply.github.com>
2019-01-07 07:18:21 +1030

    Made some movement scripts.
    Checking to see if I can use the mouse or not

---
+++ BR game/Assets/MotorForFlyingForMK.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorForFlyingForMK : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+    private Rigidbody rb;
+    private Vector3 rotation = Vector3.zero; 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+    // gets a movement vector
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity; 
+    }
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation; 
+    }
+
+    void FixedUpdate()
+    {
+        PreformMovement();
+        PreformRotation(); 
+        
+    }
+
+
+    void PreformMovement()
+    {
+        if( velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.deltaTime); 
+        }
+    }
+
+    void PreformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation)); 
+    } 
+}

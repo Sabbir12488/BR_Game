ac1bdd0877879ff2731a6592672bbebda31efeb5
Sabbir12488 <sabbir12488@gmail.com>
2019-03-08 11:09:10 +0600

    nothing big

---
+++ BR game/Assets/GFX_change.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GFX_change : MonoBehaviour {
+
+    public bool tank = false;
+    public Animator anim;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+            tank = true;
+            anim.SetBool("tank", true);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            tank = false;
+            anim.SetBool("tank", false);
+        }
+
+    }
+    void GFXChange()
+    {
+        if (tank == false)
+        {
+            tank = true;
+            anim.SetBool("tank", true);
+        }
+        if (tank == true)
+        {
+            tank = false;
+            anim.SetBool("tank", false);
+        }
+    }
+}
+
+
+
